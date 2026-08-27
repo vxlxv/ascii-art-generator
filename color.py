f3725f28b273f87d8bb824551c49ed041b4ca41c
vxlxv <jonas@born.ws>
2026-08-27 12:23:34 +0200

    Create color.py

---
+++ color.py
@@ -0,0 +1,90 @@
+class Red:
+    RED1 = '\033[91m'
+    RED2 = '\033[31m'
+    RED3 = '\033[1;31m'
+    RED4 = '\033[38;5;196m'
+    RED5 = '\033[38;5;160m'
+    RED6 = '\033[38;5;124m'
+    RED7 = '\033[38;5;88m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    ITALIC = '\033[3m'
+
+
+class Green:
+    GREEN1 = '\033[92m'
+    GREEN2 = '\033[32m'
+    GREEN3 = '\033[1;32m'
+    GREEN4 = '\033[38;5;46m'
+    GREEN5 = '\033[38;5;34m'
+    GREEN6 = '\033[38;5;22m'
+    GREEN7 = '\033[38;5;28m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    ITALIC = '\033[3m'
+
+
+class Blue:
+    BLUE1 = '\033[94m'
+    BLUE2 = '\033[34m'
+    BLUE3 = '\033[1;34m'
+    BLUE4 = '\033[38;5;39m'
+    BLUE5 = '\033[38;5;27m'
+    BLUE6 = '\033[38;5;21m'
+    BLUE7 = '\033[38;5;18m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    ITALIC = '\033[3m'
+
+
+class Yellow:
+    YELLOW1 = '\033[93m'
+    YELLOW2 = '\033[33m'
+    YELLOW3 = '\033[1;33m'
+    YELLOW4 = '\033[38;5;226m'
+    YELLOW5 = '\033[38;5;220m'
+    YELLOW6 = '\033[38;5;214m'
+    YELLOW7 = '\033[38;5;172m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    ITALIC = '\033[3m'
+
+
+class Purple:
+    PURPLE1 = '\033[95m'
+    PURPLE2 = '\033[35m'
+    PURPLE3 = '\033[1;35m'
+    PURPLE4 = '\033[38;5;141m'
+    PURPLE5 = '\033[38;5;129m'
+    PURPLE6 = '\033[38;5;93m'
+    PURPLE7 = '\033[38;5;55m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    ITALIC = '\033[3m'
+
+
+class Cyan:
+    CYAN1 = '\033[96m'
+    CYAN2 = '\033[36m'
+    CYAN3 = '\033[1;36m'
+    CYAN4 = '\033[38;5;51m'
+    CYAN5 = '\033[38;5;45m'
+    CYAN6 = '\033[38;5;37m'
+    CYAN7 = '\033[38;5;30m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    ITALIC = '\033[3m'
+
+
+class Style:
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    ITALIC = '\033[3m'
+    UNDERLINE = '\033[4m'
